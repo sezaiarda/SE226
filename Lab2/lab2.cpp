ff2fd7ee118f6346d4d3c6d4bf2f7975ddadcf12
sezaiarda <muhammedardasezai@hotmail.com>
2025-02-27 14:53:21 +0300

    Update lab2.cpp

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -13,15 +13,9 @@ void _print_words(const string words[], int size, const string start_with) {
 }
 
 void _print_pattern(int n) {
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < i; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    for (int i = n - 1; i > 0; i--) {
-        for (int j = 0; j < i; j++) {
+    for (int i = 1; i <= 2 * n - 1; i++) {
+        int stars = (i <= n) ? i : 2 * n - i;
+        for (int j = 0; j < stars; j++) {
             cout << "*";
         }
         cout << endl;
